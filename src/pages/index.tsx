fce5fb3fd8e0265617b3a10507b9d2dabef972ac
euxn23 <euxn23@gmail.com>
2020-03-16 10:39:02 +0900

    feat: add initial implementation

---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function Index() {
+  return (
+    <div>
+      <p>
+        <a
+          href="https://2020.sana.natori.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          1周年爆誕記念ヌォンタート展覧会
+        </a>
+      </p>
+      <p>
+        <a
+          href="https://button.sana.natori.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          さなボタン(3 beta)
+        </a>
+      </p>
+    </div>
+  );
+}
